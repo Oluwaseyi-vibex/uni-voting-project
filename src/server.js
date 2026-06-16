76cbca3a080a6ada693de2bbe1969156cade973a
Oluwaseyifunmi <oluseyiwmwm@gmail.com>
2026-06-16 21:27:04 +0100

    Change server listen address to '0.0.0.0'

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 // import dotenv from "dotenv";
 // dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
